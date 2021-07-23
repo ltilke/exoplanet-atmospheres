a3475e777ecdd9bdf5143bd20f75cdb6ec87c73f
ltilke <lana.tilke@icloud.com>
2021-07-23 14:03:54 -0400

    Fixed lake fraction code. :)

--- TilkeFractionalHabitability.py
+++ TilkeFractionalHabitability.py
@@ -45,21 +45,25 @@ def main():
 def get_habitability(ds, file):
     # The Basics
     ocean_depth = bath.zocean.data
-    areas = ds.axyp.data
+    areas = ds.axyp.data  # m^2
+    ice_thicknesses = ds.ZSI.data  # m
+    lake_fraction = ds.lakefr.data  # %
     water_availability = ds.avail_water_all.data
-    temps = ds.tsurf.data
-    ice_thicknesses = ds.ZSI.data
-    lake_fraction = ds.lakefr.data
-    lake_mass = ds.mwl.data
+    temps = ds.tsurf.data  # C
+    lake_mass = ds.mwl.data  # 10^10 kg
 
-    rho_water = 997
+    ice_thicknesses = np.where(np.isnan(ice_thicknesses), 0.0, ice_thicknesses)
+    lake_fraction = np.true_divide(lake_fraction, 100)
+    lake_mass *= 10**10  # kg
+
     continental_height = 30
     min_water_availability = 0.0
     hab_min_temp = 0.0
     hab_max_temp = 100.0
+    rho_water = 997.0  # kg/m^3
     land_layer_heights = [0.1, 0.173, 0.298, 0.514, 0.886, 1.529]
 
-    # Land vs. Ocean
+    # Making Masks
     land_mask = np.where(ocean_depth <= continental_height, True, False)
     land_area = np.where(land_mask, areas, 0.0)
     land_area_total = np.sum(land_area)
@@ -69,26 +73,11 @@ def get_habitability(ds, file):
     ocean_area_total = np.sum(ocean_area)
 
     has_ice_mask = np.where(np.isnan(ice_thicknesses), False, True)
+    ocean_has_ice_mask = has_ice_mask & ocean_mask
     lake_mask = np.where(lake_fraction > 0.0, True, False)
 
     water_availability_mask = np.where(np.abs(water_availability) > min_water_availability, True, False)
     water_availability_mask |= lake_mask
-
-    # Lake Habitability
-    with np.errstate(divide="ignore", invalid="ignore"):
-        lake_volume = np.where(lake_mask,
-                               np.true_divide(np.true_divide(lake_mass, np.true_divide(lake_fraction, 100)), rho_water),
-                               0.0)
-
-    lake_has_ice_mask = has_ice_mask & lake_mask
-
-    lake_ice_volume = np.where(lake_has_ice_mask, areas * ice_thicknesses, 0.0)
-    lake_ice_volume_subsurface = 9 * lake_ice_volume
-    lake_volume_total = np.sum(lake_volume) + np.sum(lake_ice_volume)
-
-    hab_lake_volume = lake_volume - lake_ice_volume_subsurface
-    hab_lake_volume_total = np.sum(np.where(hab_lake_volume > 0.0, hab_lake_volume, 0.0))
-    # This was returning a big negative number, possibly due to some lakes being entirely ice/the ice thickness being
 
     # Land Surface Area Habitability
     hab_land_area_mask = np.copy(water_availability_mask)
@@ -104,7 +93,7 @@ def get_habitability(ds, file):
     hab_ocean_area = np.where(hab_ocean_area_mask, areas, 0.0)
     hab_ocean_area_total = np.sum(hab_ocean_area)
 
-    # Land Volumetric Habitability
+    # Land Volumetric Habitability (Including Lakes)
     land_layer_temps = [ds.bs_tlay1, ds.bs_tlay2, ds.bs_tlay3, ds.bs_tlay4, ds.bs_tlay5, ds.bs_tlay6]
     land_layer_temps = [a.data for a in land_layer_temps]
     land_layer_temps = np.stack(land_layer_temps, axis=0)
@@ -112,6 +101,9 @@ def get_habitability(ds, file):
     hab_land_layer_volume_mask = np.repeat(water_availability_mask[np.newaxis, ...], land_layer_temps.shape[0], axis=0)
     hab_land_layer_volume_mask &= np.where(land_layer_temps > hab_min_temp, True, False)
     hab_land_layer_volume_mask &= np.where(land_layer_temps < hab_max_temp, True, False)
+
+    lake_volume = np.true_divide(lake_mass, rho_water)
+    lake_volume_total = np.sum(lake_volume)
 
     land_layer_volume = [a * land_area for a in land_layer_heights]
     land_layer_volume = np.stack(land_layer_volume, axis=0)
@@ -122,11 +114,9 @@ def get_habitability(ds, file):
     hab_land_layer_volume = np.where(hab_land_layer_volume_mask, land_layer_volume, 0.0)
     hab_land_volume = np.sum(hab_land_layer_volume, axis=0)
     hab_land_volume_total = np.sum(hab_land_volume)
-    hab_land_volume_total += hab_lake_volume_total
+    hab_land_volume_total += lake_volume_total
 
     # Ocean Volumetric Habitability
-    ocean_has_ice_mask = has_ice_mask & ocean_mask
-
     ocean_volume = ocean_area * ocean_depth
     ocean_volume_total = np.sum(ocean_volume)
 
