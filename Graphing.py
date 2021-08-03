4a9fc56fd72acf414bfdc5f43c13bc671bfcbbc1
ltilke <lana.tilke@icloud.com>
2021-08-03 15:48:34 -0400

    Added opacity.

--- Graphing.py
+++ Graphing.py
@@ -17,15 +17,15 @@ sdl_ticks = np.unique(sdl_ticks)
 ax = df.plot(kind="scatter",
              x="sdl",
              y="avg_hab_fraction_ocean_volume",
-             color="c", marker=".", s=216.0, label="Ocean")
+             color="c", marker=".", s=216.0, label="Ocean", alpha=0.5)
 df.plot(kind="scatter",
         x="sdl",
         y="avg_hab_fraction_land_volume",
-        color="y", marker=".", s=108.0, label="Land", ax=ax)
+        color="y", marker=".", s=108.0, label="Land", ax=ax, alpha=0.5)
 df.plot(kind="scatter",
         x="sdl",
         y="avg_hab_fraction_volume",
-        color="m", marker=".", s=36.0, label="Total", ax=ax)
+        color="m", marker=".", s=36.0, label="Total", ax=ax, alpha=1.0)
 
 ax.set_xscale("log")
 plt.ylim(bottom=-0.05, top=1.05)
@@ -41,15 +41,15 @@ plt.show()
 ax = df.plot(kind="scatter",
              x="sdl",
              y="avg_hab_fraction_ocean_area",
-             color="c", marker=".", s=216.0, label="Ocean")
+             color="c", marker=".", s=216.0, label="Ocean", alpha=0.5)
 df.plot(kind="scatter",
         x="sdl",
         y="avg_hab_fraction_land_area",
-        color="y", marker=".", s=108.0, label="Land", ax=ax)
+        color="y", marker=".", s=108.0, label="Land", ax=ax, alpha=0.5)
 df.plot(kind="scatter",
         x="sdl",
         y="avg_hab_fraction_area",
-        color="m", marker=".", s=36.0, label="Total", ax=ax)
+        color="m", marker=".", s=36.0, label="Total", ax=ax, alpha=1.0)
 
 ax.set_xscale("log")
 plt.ylim(bottom=-0.05, top=1.05)
@@ -66,15 +66,15 @@ plt.show()
 ax = df.plot(kind="scatter",
              x="insolation",
              y="avg_hab_fraction_ocean_volume",
-             color="c", marker=".", s=216.0, label="Ocean")
+             color="c", marker=".", s=216.0, label="Ocean", alpha=0.5)
 df.plot(kind="scatter",
         x="insolation",
         y="avg_hab_fraction_land_volume",
-        color="y", marker=".", s=108.0, label="Land", ax=ax)
+        color="y", marker=".", s=108.0, label="Land", ax=ax, alpha=0.5)
 df.plot(kind="scatter",
         x="insolation",
         y="avg_hab_fraction_volume",
-        color="m", marker=".", s=36.0, label="Total", ax=ax)
+        color="m", marker=".", s=36.0, label="Total", ax=ax, alpha=1.0)
 
 plt.ylim(bottom=-0.05, top=1.05)
 ax.set_xlabel(r"Insolation (W/m$^2$)")
@@ -87,15 +87,15 @@ plt.show()
 ax = df.plot(kind="scatter",
              x="insolation",
              y="avg_hab_fraction_ocean_area",
-             color="c", marker=".", s=216.0, label="Ocean")
+             color="c", marker=".", s=216.0, label="Ocean", alpha=0.5)
 df.plot(kind="scatter",
         x="insolation",
         y="avg_hab_fraction_land_area",
-        color="y", marker=".", s=108.0, label="Land", ax=ax)
+        color="y", marker=".", s=108.0, label="Land", ax=ax, alpha=0.5)
 df.plot(kind="scatter",
         x="insolation",
         y="avg_hab_fraction_area",
-        color="m", marker=".", s=36.0, label="Total", ax=ax)
+        color="m", marker=".", s=36.0, label="Total", ax=ax, alpha=1.0)
 
 plt.ylim(bottom=-0.05, top=1.05)
 ax.set_xlabel(r"Insolation (W/m$^2$)")
