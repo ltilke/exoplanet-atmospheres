ff5042a71e5c657479a1c22133d0837bca7dda7d
ltilke <lana.tilke@icloud.com>
2021-08-03 15:37:10 -0400

    Fixed formatting (titles, axes labels, marker sizes).

--- Graphing.py
+++ Graphing.py
@@ -17,21 +17,21 @@ sdl_ticks = np.unique(sdl_ticks)
 ax = df.plot(kind="scatter",
              x="sdl",
              y="avg_hab_fraction_ocean_volume",
-             color="c", marker="o", label="Ocean")
+             color="c", marker=".", s=216.0, label="Ocean")
 df.plot(kind="scatter",
         x="sdl",
         y="avg_hab_fraction_land_volume",
-        color="y", marker="o", label="Land", ax=ax)
+        color="y", marker=".", s=108.0, label="Land", ax=ax)
 df.plot(kind="scatter",
         x="sdl",
         y="avg_hab_fraction_volume",
-        color="m", marker=".", label="Total", ax=ax)
+        color="m", marker=".", s=36.0, label="Total", ax=ax)
 
 ax.set_xscale("log")
 plt.ylim(bottom=-0.05, top=1.05)
-ax.set_xlabel("Sidereal Day Length")
+ax.set_xlabel(r"Sidereal Day Length (SDL$_\oplus$)")
 ax.set_ylabel("Habitable Fraction")
-plt.title(r"Habitable Fraction (Volume) vs. Sidereal Day Length (SDL$_\oplus$)")
+plt.title("Habitable Fraction (Volume) vs. Sidereal Day Length")
 plt.xticks(ticks=sdl_ticks, labels=sdl_ticks)
 plt.minorticks_off()
 plt.legend(loc="best")
@@ -41,21 +41,21 @@ plt.show()
 ax = df.plot(kind="scatter",
              x="sdl",
              y="avg_hab_fraction_ocean_area",
-             color="c", marker="o", label="Ocean")
+             color="c", marker=".", s=216.0, label="Ocean")
 df.plot(kind="scatter",
         x="sdl",
         y="avg_hab_fraction_land_area",
-        color="y", marker="o", label="Land", ax=ax)
+        color="y", marker=".", s=108.0, label="Land", ax=ax)
 df.plot(kind="scatter",
         x="sdl",
         y="avg_hab_fraction_area",
-        color="m", marker=".", label="Total", ax=ax)
+        color="m", marker=".", s=36.0, label="Total", ax=ax)
 
 ax.set_xscale("log")
 plt.ylim(bottom=-0.05, top=1.05)
-ax.set_xlabel("Sidereal Day Length")
+ax.set_xlabel(r"Sidereal Day Length (SDL$_\oplus$)")
 ax.set_ylabel("Habitable Fraction")
-plt.title(r"Habitable Fraction (Area) vs. Sidereal Day Length (SDL$_\oplus$)")
+plt.title("Habitable Fraction (Area) vs. Sidereal Day Length")
 plt.xticks(ticks=sdl_ticks, labels=sdl_ticks)
 plt.minorticks_off()
 plt.legend(loc="best")
@@ -66,20 +66,20 @@ plt.show()
 ax = df.plot(kind="scatter",
              x="insolation",
              y="avg_hab_fraction_ocean_volume",
-             color="c", marker="o", label="Ocean")
+             color="c", marker=".", s=216.0, label="Ocean")
 df.plot(kind="scatter",
         x="insolation",
         y="avg_hab_fraction_land_volume",
-        color="y", marker="o", label="Land", ax=ax)
+        color="y", marker=".", s=108.0, label="Land", ax=ax)
 df.plot(kind="scatter",
         x="insolation",
         y="avg_hab_fraction_volume",
-        color="m", marker=".", label="Total", ax=ax)
+        color="m", marker=".", s=36.0, label="Total", ax=ax)
 
 plt.ylim(bottom=-0.05, top=1.05)
-ax.set_xlabel("Insolation")
+ax.set_xlabel(r"Insolation (W/m$^2$)")
 ax.set_ylabel("Habitable Fraction")
-plt.title(r"Habitable Fraction (Volume) vs. Insolation (W/m$^2$)")
+plt.title("Habitable Fraction (Volume) vs. Insolation")
 plt.legend(loc="best")
 plt.savefig("figs\\volume_insolation.png", dpi=600)
 plt.show()
@@ -87,20 +87,20 @@ plt.show()
 ax = df.plot(kind="scatter",
              x="insolation",
              y="avg_hab_fraction_ocean_area",
-             color="c", marker="o", label="Ocean")
+             color="c", marker=".", s=216.0, label="Ocean")
 df.plot(kind="scatter",
         x="insolation",
         y="avg_hab_fraction_land_area",
-        color="y", marker="o", label="Land", ax=ax)
+        color="y", marker=".", s=108.0, label="Land", ax=ax)
 df.plot(kind="scatter",
         x="insolation",
         y="avg_hab_fraction_area",
-        color="m", marker=".", label="Total", ax=ax)
+        color="m", marker=".", s=36.0, label="Total", ax=ax)
 
 plt.ylim(bottom=-0.05, top=1.05)
-ax.set_xlabel("Insolation")
+ax.set_xlabel(r"Insolation (W/m$^2$)")
 ax.set_ylabel("Habitable Fraction")
-plt.title(r"Habitable Fraction (Area) vs. Insolation (W/m$^2$)")
+plt.title("Habitable Fraction (Area) vs. Insolation")
 plt.legend(loc="best")
 plt.savefig("figs\\area_insolation.png", dpi=600)
 plt.show()
